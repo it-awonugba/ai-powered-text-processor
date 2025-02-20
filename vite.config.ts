48f14fb6782b7c8953062bdcfb5d9b529d0796c6
WebKing <isaac@afrocenchix.com>
2025-02-20 12:44:17 +0000

    Bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   define: {
-    "globalThis.ai": "globalThis.ai || {}",
+    "globalThis.ai": "{}",
   },
   plugins: [react()],
 });
